bd543b2b42ac8df4982df300445fe2b9a9d9840e
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-11-30 21:27:49 +0100

    trust proxy

--- express5/app.js
+++ express5/app.js
@@ -12,13 +12,25 @@ const mongo_url = process.env.MONGO_URI
 const authMiddleware = require('./middlewares/auth')
 const notFoundMiddleware = require('./middlewares/notfound')
 const errorHandlerMiddleware = require('./middlewares/errorhandler')
-// const userValidate = require('./middlewares/userValidate')
+const rateLimit = require('express-rate-limit')
+
+const limit = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes //ms means miliseconds and window represents the sessions
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    message : 'Too many requests sent using this IP address please try again later',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limit)
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 app.use(cookieParser())
 
+app.set('trust proxy', 1)
 app.use('/', authRouter)
 app.use('/', authMiddleware, budgetRouter)
 

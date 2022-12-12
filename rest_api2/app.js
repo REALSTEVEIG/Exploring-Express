c2dd2869d1001bf4f19360e37dc95d7a3cfd43de
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-12-12 22:21:39 +0100

    rate limitter

--- rest_api2/app.js
+++ rest_api2/app.js
@@ -8,6 +8,23 @@ const connectDB = require('./db/connect')
 const authRouter = require('./routes/auth')
 const budgetRouter = require('./routes/budget')
 
+//SECURITY
+const cors = require('cors')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+
+const limit = rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes //ms means miliseconds and window represents the sessions
+    max: 5, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+    message : 'Too many requests sent using this IP address please try again later',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.set('trust proxy', 1)
+app.use(cors())
+app.use(helmet())
+app.use(limit)
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 

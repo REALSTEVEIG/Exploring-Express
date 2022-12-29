897336d5b95712db909e0e434b32e926839c3274
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-12-29 21:28:08 +0100

    rate limiting for login

--- passport_api/routes/user.js
+++ passport_api/routes/user.js
@@ -4,8 +4,20 @@ const router = express.Router()
 const {register, login, logout} = require('../controllers/users')
 const userValidation = require('../middleware/userValidation')
 
+const rateLimit = require('express-rate-limit')
+
+//rate-limiting
+const limiter = rateLimit({
+	windowMs: 60 * 1000, // 1 minutes
+	max: 5, // Limit each IP to 5 requests per `window` (here, per 1 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message:
+		'Too many logins initiated from this IP, please try again after a minute'
+})
+
 router.route('/register').post(userValidation, register)
-router.route('/login').post(login)
+router.route('/login').post(limiter, login)
 router.route('/logout').get(logout)
 
 module.exports = router
